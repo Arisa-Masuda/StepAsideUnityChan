ba5fa2e8d42f5f7d35fc31f59bbf196b699a6e26
Arisa-Masuda <119154969+Arisa-Masuda@users.noreply.github.com>
2023-01-03 11:04:41 +0900

    Lesson6課題の修正
    Lesson6の発展課題を再修正

--- Assets/ItemGenerator.cs
+++ Assets/ItemGenerator.cs
@@ -23,9 +23,7 @@ public class ItemGenerator : MonoBehaviour
     //Unityちゃんの見える範囲
     private float viewPos = 40f;
     //アイテムを生成する開始地点
-    private float startCreatePos = 0f;
-    //アイテムを生成する終了地点
-    private float endCreatePos = 0f;
+    private float Pos = 0f;
     //前回アイテムが生成された終了地点
     private float createdPos = 0f;
 
@@ -40,84 +38,57 @@ public class ItemGenerator : MonoBehaviour
     void Update()
     {
         //Unityちゃんの位置＋４０ｍ先をアイテム生成の終了位置に設定
-        endCreatePos = unitychan.transform.position.z + viewPos;
+        Pos = unitychan.transform.position.z + viewPos;
 
-        //アイテム生成の終了位置がスタート地点を超えている場合
-        if (endCreatePos > startPos)
+        //前回アイテムを生成した位置+15fを超えている場合
+        if (Pos >= createdPos + 15f && Pos < goalPos)
         {
-            //アイテム生成の終了位置が前回アイテムを作成した位置を超えているかつ
-            //前回アイテムを作成した位置がゴール地点を超えていない場合
-            if (endCreatePos > createdPos && createdPos < goalPos)
-            {
-                //初回アイテム生成の場合
-                if (startCreatePos == 0f)
-                {
-                    startCreatePos = startPos;       //スタート地点を設定
-                    endCreatePos = startPos += 15f;  //スタート地点＋15m先を設定
-                }
-                //アイテム生成の終了位置がゴール地点を超えている場合
-                else if (endCreatePos > goalPos)
-                {
-                    startCreatePos = createdPos;  //前回アイテムを作成した位置
-                    endCreatePos = goalPos;       //ゴール地点を設定
-                }
-                else
-                {
-                    startCreatePos = createdPos;  //前回アイテムを作成した位置
-                    if (endCreatePos - startCreatePos <= 10f) //前回アイテムを作成した位置と今回作成する位置の差が10f以下の場合
-                    {
-                        endCreatePos += 10f;     //今回作成する位置に加算する
-                    }
-                }
-
-                //アイテムを生成する
-                CreateItem(startCreatePos, endCreatePos);
-                //アイテムを生成した終了位置を保持する
-                createdPos = endCreatePos;
-
-            }
+            //アイテムを生成する
+            CreateItem(Pos);
+            //アイテムを生成した終了位置を保持する
+            createdPos = Pos;
         }
+
     }
 
     //アイテムを作成する処理
-    void CreateItem(float posS, float posE)
+    void CreateItem(float pos)
     {
-        for (float i = posS; i < posE; i += 15f)
+        //どのアイテムを出すのかをランダムに設定
+        int num = Random.Range(1, 6);
+        //どのレーンにいくつアイテムを出すのかランダムに設定
+        int lane = Random.Range(-1, 1);
+
+        //コーンを生成する
+        if (num <= 1)
         {
-            //どのアイテムを出すのかをランダムに設定
-            int num = Random.Range(1, 11);
-            if (num <= 2)
+            //コーンをx軸方向に一直線に生成
+            for (float j = -1; j <= 1; j += 0.4f)
             {
-                //コーンをx軸方向に一直線に生成
-                for (float j = -1; j <= 1; j += 0.4f)
-                {
-                    GameObject cone = Instantiate(conePrefab);
-                    cone.transform.position = new Vector3(4 * j, cone.transform.position.y, i);
-                }
+                GameObject cone = Instantiate(conePrefab);
+                cone.transform.position = new Vector3(4 * j, cone.transform.position.y, pos);
             }
-            else
+        }
+        //コインを生成する
+        else if (2 <= num && num <= 4)
+        {
+            //レーンごとにアイテムを生成
+            for (int j = lane; j <= 1; j++)
             {
-                //レーンごとにアイテムを生成
-                for (int j = -1; j <= 1; j++)
-                {
-                    //アイテムの種類を決める
-                    int item = Random.Range(1, 11);
-                    //アイテムを置くℤ座標のオフセットをランダムに設定
-                    int offsetZ = Random.Range(-5, 6);
-                    //６０％コインを配置：３０％車を配置：１０％何もなし
-                    if (1 <= item && item <= 6)
-                    {
-                        //コインを生成（Prefabからインスタンスを生成している）
-                        GameObject coin = Instantiate(coinPrefab); //Instantiate () は、()内に指定したPrefabのインスタンスをGameObject型として生成する
-                        coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, i + offsetZ);
-                    }
-                    else if (7 <= item && item <= 9)
-                    {
-                        //車を生成
-                        GameObject car = Instantiate(carPrefab);
-                        car.transform.position = new Vector3(posRange * j, car.transform.position.y, i + offsetZ);
-                    }
-                }
+                //コインを生成（Prefabからインスタンスを生成している）
+                GameObject coin = Instantiate(coinPrefab); //Instantiate () は、()内に指定したPrefabのインスタンスをGameObject型として生成する
+                coin.transform.position = new Vector3(posRange * j, coin.transform.position.y, pos);
+            }
+        }
+        //車を生成する
+        else if (5 <= num) 
+        {
+            //レーンごとにアイテムを生成
+            for (int j = lane; j <= 1; j++)
+            {
+                //車を生成
+                GameObject car = Instantiate(carPrefab);
+                car.transform.position = new Vector3(posRange * j, car.transform.position.y, pos);
             }
         }
     }
